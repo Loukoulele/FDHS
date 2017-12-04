027c6b5d285e9e4d591ab0ca7ce9473dfa25ab35
Julien <julien1105@outlook.fr>
2017-12-04 19:07:11 +0100

    Projetweb V1.0.0 - "page authentication, side menu and page register"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    FooterComponent,
+    MenuComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
